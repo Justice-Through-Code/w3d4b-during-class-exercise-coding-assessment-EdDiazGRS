a85b575498a8e3bab7d77e573a63b6a2f1147e06
Ed Diaz <eddiaz.grs@gmail.com>
2023-09-21 14:52:02 -0400

    working code

--- geometric_shape_area_calculator.py
+++ geometric_shape_area_calculator.py
@@ -8,22 +8,22 @@ def main():
     # TODO: In terminal, print Welcome to the geometric shape area calculator!
     print('Welcome to the geometric shape area calculator!')
     # User Options
-    Circle = 1
-    Rectangle = 2
-    Triangle = 3
+    # Circle = 1
+    # Rectangle = 2
+    # Triangle = 3
     
     # TODO: Using one print statement, use string concatenation to print the options only 
     # as a single string (make sure to add a space between each option)
-    print( 'Circle' + ' ' + 'Rectangle' + ' ' + 'Triangle')
+    print('Circle = 1' + ' ' + 'Rectangle = 2' + ' ' + 'Triangle = 3')
 
     # TODO: In terminal, ask the user "Select a shape by entering 1, 2, or 3' and assign the input to a new variable named 'choice'.
      
-    print(input('Select a shape by entering 1, 2, or 3'))
-    choice = input
+    choice = (input('Select a shape by entering 1, 2, or 3 '))
+    print(choice)
     # TODO: Convert the variable 'choice' to an integer data type.
-    choice = int()
+    choice = int(choice)
     # TODO: With one line of code, verify the variable 'choice' is indeed the data type integer, use conditional logic and print the output.  If converted correctly, the output in Terminal should read 'True'.
-    print(choice == int)
+    print(type(choice) == int)
     if choice == 1:  #DO NOT REMOVE THE 'IF'
         # Calculate the area of a circle
 
@@ -39,8 +39,8 @@ def main():
     elif choice == 2: # DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a rectangle
         # TODO: Assign new variables 'length' and 'width' and ask for the user's input for the length and width of the rectangle.
-        length = input('what is the length of the rectangle?')
-        width = input('what is the width of the rectangle?')
+        length = input('what is the length of the rectangle? ')
+        width = input('what is the width of the rectangle? ')
         # TODO: Convert both 'length' and 'width' to float.
         length = float(length)
         width = float(width)
@@ -50,8 +50,8 @@ def main():
     elif choice == 3: #DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a triangle
         # TODO: Assign new variables 'base' and 'height' and ask for the user's input for the base length and height of the triangle.
-        base = input('what is the base length?')
-        height = input('what is the height?')
+        base = input('what is the base length? ')
+        height = input('what is the height? ')
         
         # TODO: Convert both 'base' and 'height' to float.
         base = float(base)
@@ -62,12 +62,12 @@ def main():
 
     else:
         # TODO: If the user enters anything other than 1, 2 or 3, print statement "Invalid choice ."
-        print('Invalid choice')
+        print('Invalid choice .')
     if choice in [1, 2, 3]: # DO NOT MODIFY
         print(f"The area is: {area:.2f} square units.") # DO NOT MODIFY
 
     # TODO: Print a statement explaining each step required to find and complete your technical assignments.  Be specific. 
-
+    print('Step_1 - Go to assignments tab and select the desired assignment.\nStep_2 - Click on the "Click Here" assignment link. \nStep_3 - Click on the JTC classroom github link and refresh the page. \nStep_4 - Copy the url link and clone the repo to your vs code editor. \nStep_5 - Complete the technical assignment.\nStep_6 - Git add, git commit, and then git push your code. \nStep_7 - Log in to your github account and copy the url and paste it in the "start assignment" text box in the assignment page.')
 
 if __name__ == "__main__": # DO NOT MODIFY
-    # main() # DO NOT MODIFY
+    main()
